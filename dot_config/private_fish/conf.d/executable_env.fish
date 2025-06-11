749069c03aae65c75588c812447c90c675b08a2c
Pinio <john.wpn@gmail.com>
2025-06-11 20:41:10 +0200

    Added carapace integration to fish config.

--- dot_config/private_fish/conf.d/executable_env.fish
+++ dot_config/private_fish/conf.d/executable_env.fish
@@ -7,3 +7,4 @@ set -gx MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -
 set -gx BAT_THEME 'ansi'
 set -gx FZF_DEFAULT_OPTS '--layout=reverse'
 set -gx zoxide_cmd 'cd'
+set -gx CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense'
--- dot_config/private_fish/conf.d/executable_integrations.fish
+++ dot_config/private_fish/conf.d/executable_integrations.fish
@@ -4,4 +4,8 @@ fzf --fish | source
 # starship
 starship init fish | source
 
+# pay-respects
 pay-respects fish --alias fuck | source
+
+# carapace
+carapace _carapace | source
