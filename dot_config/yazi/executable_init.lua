4f0818f58bfd799e27a420cd992338bb765bc86f
Pinio <john.wpn@gmail.com>
2025-06-11 19:23:52 +0200

    Added yazi config

---
+++ dot_config/yazi/executable_init.lua
@@ -0,0 +1,29 @@
+require("full-border"):setup()
+
+require("bunny"):setup({
+  hops = {
+    { key = "/", path = "/" },
+    { key = "h",  path = "/home/johnw", desc = "Home" },
+    { key = "c", path = "~/.config", desc = "Config files" },
+    -- key and path attributes are required, desc is optional
+  },
+  desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
+  ephemeral = true, -- Enable ephemeral hops, default is true
+  tabs = true, -- Enable tab hops, default is true
+  notify = false, -- Notify after hopping, default is false
+  fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
+})
+
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		" ",
+	}
+end, 500, Status.RIGHT)
